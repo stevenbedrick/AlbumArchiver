3c1475f8ac8f826d420646935e3bc4ef8d7c8a8b
Steven Bedrick <steve@bedrick.org>
2022-12-04 13:34:13 -0800

    Initial Commit

---
+++ AlbumArchiver-SwiftUI/AlbumArchiver_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumArchiver_SwiftUIApp.swift
+//  AlbumArchiver-SwiftUI
+//
+//  Created by Steven Bedrick on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct AlbumArchiver_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlbumArchiver-SwiftUIUITests/AlbumArchiver_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AlbumArchiver_SwiftUIUITestsLaunchTests.swift
+//  AlbumArchiver-SwiftUIUITests
+//
+//  Created by Steven Bedrick on 12/4/22.
+//
+
+import XCTest
+
+final class AlbumArchiver_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
